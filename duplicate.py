6cf7d03a74e1887613bcd47c3de589192679244c
danreut89 <danreut@redcoffeegames.com>
2025-02-18 17:21:02 -0500

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -4,6 +4,8 @@ def greet():
     for _ in range(10):  # Increase duplicate block size
         print("Hello, SonarCloud!")
     print("Testing SonarCloud duplication detection.")
+    print("Testing SonarCloud duplication detection.")
+
 
 
 if __name__ == "__main__":
